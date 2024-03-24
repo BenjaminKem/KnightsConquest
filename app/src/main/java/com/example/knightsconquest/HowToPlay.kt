a3cbc6fd624a7815f05040d130cef706f0dc3180
Erik Berner <erik.berner@reutlingen-university.de>
2024-03-24 18:53:27 +0100

    updated HowToPlay background

--- app/src/main/java/com/example/knightsconquest/HowToPlay.kt
+++ app/src/main/java/com/example/knightsconquest/HowToPlay.kt
@@ -10,7 +10,7 @@ class HowToPlay : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_how_to_play)
+        setContentView(R.layout.activity_how_to_play1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
