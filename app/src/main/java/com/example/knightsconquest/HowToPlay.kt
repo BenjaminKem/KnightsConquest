f80f4cce8b57ead62f5d133c981970228778193d
Erik Berner <erik.berner@reutlingen-university.de>
2024-03-24 19:04:26 +0100

    fixed an error

--- app/src/main/java/com/example/knightsconquest/HowToPlay.kt
+++ app/src/main/java/com/example/knightsconquest/HowToPlay.kt
@@ -1,5 +1,6 @@
 package com.example.knightsconquest
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,10 +8,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HowToPlay : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_how_to_play1)
+        setContentView(R.layout.activity_how_to_play)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
